f03b19db10f1772d36ea0ffbd7a73ce156196852
Diego Barrales <diego.barrales@csu.fullerton.edu>
2024-03-18 17:33:36 -0700

    Next/Previous buttons + code cleanup

--- js/quiz.js
+++ js/quiz.js
@@ -38,19 +38,71 @@ class Quiz {
     //     "possibleAnswers": ["good", "bad", "fine", "ok"],
     //     "correctAnswer": 0,
     // }
+    //      "answerChoice" will be defined by the quiz class as the user answer questions
     constructor(questions) {
         this.questions = questions;
     }
 
+    start() {
+        this.setUpQuestion();
+    }
+
+    setUpQuestion() {
+        this.displayCurrentQuestion();
+        this.setEventListener();
+    }
+
+    updateQuestion(isCorrect) {
+        this.score += (isCorrect) ? 1 : 0;
+        // this.currentQuestion += 1;
+        this.nextQuestion()
+
+        debugger;
+        if (this.quizFinished()) {
+            // TODO:endQuiz();
+            $('#quizCompleted').modal('toggle');
+            return;
+        }
+
+        this.setUpQuestion();
+    }
+
+    // Logical Helpers ****************************************
+    getCorrectAnswer() {
+        return this.questions[this.currentQuestion].correctAnswer;
+    }
+
+    quizFinished() {
+        // quiz finishes when all questions have been answered
+        for (let i = 0; i < this.questions.length; i++) {
+            if (!this.questions[i].answerChoice) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // End of Logical Helpers ****************************************
+
+    // Render Functions ****************************************
     displayAnswers() {
         let answersHTML = "<div class='row'>";
 
         let answers = this.questions[this.currentQuestion].possibleAnswers;
         answers.forEach((answer, index) => {
+            let color = "primary";
+            const userChoice = this.questions[this.currentQuestion].answerChoice;
+            if (userChoice) {
+                if (this.getCorrectAnswer() == userChoice) {
+                    color = "success";
+                } else {
+                    color = "danger";
+                }
+            }
 
             answersHTML +=
                 `<div class="col-6">
-                <button class="btn btn-primary m-1 px-4 py-3 w-100" type="button" data-choice="${index}">
+                <button class="btn btn-${(userChoice == index) ? color : "primary"} m-1 px-4 py-3 w-100" type="button" data-choice="${index}">
                     ${answer}
                 </button>
             </div>`;
@@ -71,27 +123,15 @@ class Quiz {
         this.displayAnswers();
     }
 
-    getCorrectAnswer() {
-        return this.questions[this.currentQuestion].correctAnswer;
-    }
+    // End of Render Functions ****************************************
 
-    updateQuestion(isCorrect) {
-        this.score += (isCorrect) ? 1 : 0;
-        this.currentQuestion += 1;
-        
-        if (this.currentQuestion >= this.questions.length) {
-            // TODO:endQuiz();
-            return;
-        }
-        debugger;
-
-        this.setUpQuestion();
-    }
-
-
+    // Event Handlers ****************************************
     answerClicked = (event) => {
         event.preventDefault();
         if (event.target.nodeName != "BUTTON") { return; }
+
+        if (this.questions[this.currentQuestion].answerChoice) { return; }
+
         const choice = event.target.dataset.choice;
 
         const isCorrect = (parseInt(choice) == this.getCorrectAnswer());
@@ -100,23 +140,35 @@ class Quiz {
         } else {
             showMessage("Incorrect!", "danger");
         }
-        this.updateQuestion();
+
+        this.questions[this.currentQuestion].answerChoice = choice;
+
+        this.updateQuestion(isCorrect);
+    }
+
+    nextQuestion = (event) => {
+        if (event) { event.preventDefault(); }
+
+        this.currentQuestion = Math.min(this.questions.length - 1, this.currentQuestion + 1);
+        this.setUpQuestion();
+    }
+
+    previousQuestion = (event) => {
+        if (event) { event.preventDefault(); }
+
+        this.currentQuestion = Math.max(0, this.currentQuestion - 1);
+        this.setUpQuestion();
     }
 
     setEventListener() {
         let answerContainer = document.querySelector("#answerDisplay");
-
         answerContainer.addEventListener("click", this.answerClicked);
+
+        let previousBtn = document.querySelector("#previousBtn");
+        previousBtn.addEventListener("click", this.previousQuestion);
+
+        let nextBtn = document.querySelector("#nextBtn");
+        nextBtn.addEventListener("click", this.nextQuestion);
     }
-
-    setUpQuestion() {
-        this.displayCurrentQuestion();
-
-        this.setEventListener();
-    }
-
-    start() {
-        this.setUpQuestion();
-    }
-
+    // End of Event Handlers ****************************************
 }
